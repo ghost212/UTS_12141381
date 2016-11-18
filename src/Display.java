c7a0274977eaee03a2e104ff704244e26f2a5dfd
USER <USER@192.168.43.111>
2016-11-18 20:57:31 +0700

    12141381 Display

---
+++ src/Display.java
@@ -0,0 +1,10 @@
+
+import com.rizki.app12141381.Tugas;
+
+public class Display { 
+    public static void main(String[] args) {
+        Tugas game = new Tugas();
+        game.Games();
+    }
+    
+}

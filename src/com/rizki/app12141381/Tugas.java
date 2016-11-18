5cab40234fcc0c03c784f5ada9da2dd852095d0a
USER <USER@192.168.43.111>
2016-11-18 20:38:15 +0700

    12141381 Attribut Class

---
+++ src/com/rizki/app12141381/Tugas.java
@@ -0,0 +1,16 @@
+package com.rizki.app12141381;
+
+/**
+ *
+ * @author USER
+ */
+public class Tugas {
+    public String nama;
+    public int asalAngka;
+    public byte level;
+    public char jawab;
+    public int angkaAcak;
+    public int kesempatan;
+    
+    
+}
